6456002070cdf1673f1b64be9bbcca941fd416b7
sgrim3 <susan.grimshaw@students.olin.edu>
2015-03-28 09:40:59 -0400

    Added Events and People schemas

---
+++ models/models.js
@@ -0,0 +1,31 @@
+/* 
+	The db schema for a topic in our wiki
+	img is a string of a url so the picture
+	is not a file upload because we don't want to store
+	all that in our db
+*/
+
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var EventsSchema = new Schema({
+	name: String,
+	date: Date,
+	description: String,
+	code: String,
+	people: [{ type: Schema.ObjectId, ref: 'People' }]
+});
+
+var PeopleSchema = new Schema({
+    //username : { type: Schema.ObjectId, ref: 'Events' },
+    name: String,
+	description: String,
+	tags: [String],
+	picture : String
+});
+
+
+module.exports.events = mongoose.model("Events", EventsSchema);
+module.exports.twote = mongoose.model("People", PeopleSchema);
+
+ 
